845c78b116b1074200c4a4d198aeddd8cfbc0376
Max Kremmel <xing@synapse.plus.com>
2005-12-10 10:53:27 +0000

    use PKG_DIR as key for registerAppMenu

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -4,6 +4,6 @@ global $gBitSystem;
 $gBitSystem->registerPackage( 'Newsletters',  dirname( __FILE__ ).'/' );
 
 if( $gBitSystem->isPackageActive( NEWSLETTERS_PKG_NAME ) ) {
-	$gBitSystem->registerAppMenu( NEWSLETTERS_PKG_DIR, NEWSLETTERS_PKG_NAME, NEWSLETTERS_PKG_URL.'index.php', 'bitpackage:newsletters/menu_newsletters.tpl', true );
+	$gBitSystem->registerAppMenu( NEWSLETTERS_PKG_DIR, 'Newsletters', NEWSLETTERS_PKG_URL.'index.php', 'bitpackage:newsletters/menu_newsletters.tpl', NEWSLETTERS_PKG_NAME );
 }
 ?>
