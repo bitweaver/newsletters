1b9eead501696cbf466329be9abba959d3d673a2
Christian Fowler <spider@viovio.com>
2005-12-29 21:08:24 +0000

    first pass at rough table re-organization for upgrade

--- admin/upgrade_inc.php
+++ admin/upgrade_inc.php
@@ -7,18 +7,39 @@ $upgrades = array(
 	'CLYDE' => array(
 array( 'RENAMETABLE' => array(
         'tiki_sent_newsletters' => 'tiki_newsletters_editions',
+        'tiki_newsletter_subscriptions' => 'tiki_mail_subscriptions',
 )),
 // STEP 1
 array( 'DATADICT' => array(
-array( 'RENAMECOLUMN' => array(
-	'tiki_quicktags' => array( '`tagId`' => '`tag_id` I4 AUTO' ),
-)),
-array( 'ALTER' => array(
-	'tiki_quicktags' => array(
-		'tagpos' => array( '`tagpos`', 'I4' ), // , 'NOTNULL' ),
-		'format_guid' => array( '`format_guid`', 'VARCHAR(16)' ), // , 'NOTNULL' ),
-	),
-))
+	array( 'RENAMECOLUMN' => array(
+		'tiki_newsletters' => array( '`nlId`' => '`nl_id` I4 AUTO' ),
+		'tiki_newsletters' => array( '`allowUserSub`' => "`allow_user_sub` C(1) default 'y'" ),
+		'tiki_newsletters' => array( '`allowAnySub`' => '`allow_any_sub` C(1)' ),
+		'tiki_newsletters' => array( '`unsubMsg`' => "`unsub_msg` C(1) default 'y'" ),
+		'tiki_newsletters' => array( '`validateAddr`' => "`validate_addr` C(1) default 'y'" ),
+		'tiki_newsletters' => array( '`lastSent`' => '`last_sent` I8' ),
+		'tiki_newsletters_editions' => array( '`editionId`' => '`edition_id` I4 AUTO' ),
+	)),
+	// ALTER
+	array( 'ALTER' => array(
+		'tiki_newsletters' => array(
+			'content_id' => array( '`content_id`', 'I4' ),
+		),
+		'tiki_newsletters_editions' => array(
+			'content_id' => array( '`content_id`', 'I4' ),
+			'nl_content_id' => array( '`nl_content_id`', 'I4' ),
+			'is_draft' => array( '`is_draft`', 'C(1)' ),
+		),
+		'tiki_newsletter_subscriptions' => array(
+			'nl_content_id' => array( '`nl_content_id`', 'I4' ),
+			'response_content_id' => array( '`response_content_id`', 'I4' ),
+			'unsubscribe_all' => array( '`unsubscribe_all`', 'C(1)' ),
+			'unsubscribe_date' => array( '`unsubscribe_date`', 'I8' ),
+		),
+	)),
+	// CREATE
+	array( 'CREATE' => array (
+	)),
 )),
 
 // STEP 3
@@ -27,6 +48,13 @@ array( 'QUERY' =>
 		),
 )),
 
+// STEP 6
+array( 'DATADICT' => array(
+	array( 'DROPCOLUMN' => array(
+		'tiki_newsletters' => array( '`name`', '`description`', '`created`', '`users`', '`editions`' ),
+		'tiki_newsletter_subscriptions' => array( '`code`', '`valid`', '`subscribed`' ),
+	)),
+)),
 	)
 )
 );
