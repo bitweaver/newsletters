cfb71170e310f84f6aa082672e7cd9ba9067cc57
Max Kremmel <xing@synapse.plus.com>
2006-02-01 19:01:48 +0000

    unify table aliases

--- BitNewsletterEdition.php
+++ BitNewsletterEdition.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_newsletters/BitNewsletterEdition.php,v 1.18 2006/02/01 18:42:23 squareing Exp $
+ * $Header: /cvsroot/bitweaver/_bit_newsletters/BitNewsletterEdition.php,v 1.19 2006/02/01 19:01:48 squareing Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: BitNewsletterEdition.php,v 1.18 2006/02/01 18:42:23 squareing Exp $
+ * $Id: BitNewsletterEdition.php,v 1.19 2006/02/01 19:01:48 squareing Exp $
  *
  * Class that handles editions of newsletters
  * @package newsletters
@@ -15,7 +15,7 @@
  *
  * @author spiderr <spider@steelsun.com>
  *
- * @version $Revision: 1.18 $ $Date: 2006/02/01 18:42:23 $ $Author: squareing $
+ * @version $Revision: 1.19 $ $Date: 2006/02/01 19:01:48 $ $Author: squareing $
  */
 
 /**
@@ -151,11 +151,11 @@ class BitNewsletterEdition extends LibertyAttachable {
 			$bindVars[] = $findesc;
 			$bindVars[] = $findesc;
 		}
-		$query = "SELECT `edition_id` AS `hash_key`, ne.*, lc.*, tc2.`title` AS `newsletter_title`
+		$query = "SELECT `edition_id` AS `hash_key`, ne.*, lc.*, lc2.`title` AS `newsletter_title`
 				  FROM `".BIT_DB_PREFIX."newsletters_editions` ne
 				  	INNER JOIN `".BIT_DB_PREFIX."liberty_content` lc ON( lc.`content_id`=ne.`content_id` )
 				  	INNER JOIN `".BIT_DB_PREFIX."newsletters` n ON( ne.`nl_content_id`=n.`content_id` )
-				  	LEFT OUTER JOIN `".BIT_DB_PREFIX."liberty_content` tc2 ON( n.`content_id`=tc2.`content_id` )
+				  	LEFT OUTER JOIN `".BIT_DB_PREFIX."liberty_content` lc2 ON( n.`content_id`=lc2.`content_id` )
 				  $mid ORDER BY ".$this->mDb->convert_sortmode( $pListHash['sort_mode'] );
 		$query_cant = "select count(*) from `".BIT_DB_PREFIX."newsletters` n INNER JOIN `".BIT_DB_PREFIX."newsletters_editions` ne ON(n.`content_id`=ne.`nl_content_id`) $mid";
 		$ret = $gBitDb->getAssoc( $query, $bindVars, $pListHash['max_records'], $pListHash['offset'] );
