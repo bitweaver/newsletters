c4d2adbd4bf15c220f909b07f996e45de6802f04
Lester Caine <lester@lsces.co.uk>
2006-02-18 07:28:47 +0000

    Update registerPackage hash - so installer can run

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -1,7 +1,11 @@
 <?php
 global $gBitSystem;
 
-$gBitSystem->registerPackage( 'Newsletters',  dirname( __FILE__ ).'/' );
+$registerHash = array(
+	'package_name' => 'newsletters',
+	'package_path' => dirname( __FILE__ ).'/',
+);
+$gBitSystem->registerPackage( $registerHash );
 
 if( $gBitSystem->isPackageActive( NEWSLETTERS_PKG_NAME ) ) {
 	$gBitSystem->registerAppMenu( NEWSLETTERS_PKG_DIR, 'Newsletters', NEWSLETTERS_PKG_URL.'index.php', 'bitpackage:newsletters/menu_newsletters.tpl', NEWSLETTERS_PKG_NAME );
