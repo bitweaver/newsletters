abdd1cdbafba523687b75848b9cfdf435448209f
Christian Fowler <spider@viovio.com>
2006-02-25 20:22:34 +0000

    remove antiquated tiki_setup_inc.php file

--- tiki_setup_inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-global $gTikiSystem;
-
-$gTikiSystem->registerPackage( 'Newsletters',  dirname( __FILE__ ).'/' );
-
-if( $gTikiSystem->isPackageActive( 'NEWSLETTERS_PKG_NAME' ) ) {
-	$gTikiSystem->registerAppMenu( NEWSLETTERS_PKG_DIR, NEWSLETTERS_PKG_NAME, NEWSLETTERS_PKG_URL.'index.php', 'tikipackage:newsletters/menu_newsletters.tpl', true );
-}
-?>
