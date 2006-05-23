c31ed1657288a03179364023bdcc4384271ea528
Stephan Borg <wolff_borg@users.sourceforge.net>
2006-05-23 11:43:54 +0000

    Allowed newsletters to be sent as anonymous once queued by an administrator.

--- admin/tend_mail_queue.php
+++ admin/tend_mail_queue.php
@@ -14,16 +14,10 @@
  */
 	if( !empty( $argc ) ) {
 		// reduce feedback for command line to keep log noise way down
-//		define( 'BIT_PHP_ERROR_REPORTING', E_ERROR | E_PARSE );
+		define( 'BIT_PHP_ERROR_REPORTING', E_ERROR | E_PARSE );
 	}
 
 	require_once( '../../bit_setup_inc.php' );
-
-	// add some protection for arbitrary thumbail execution.
-	// if argc is present, we will trust it was exec'ed command line.
-	if( empty( $argc ) && !$gBitUser->isAdmin() ) {
-		$gBitSystem->fatalError( 'You cannot run the thumbnailer' );
-	}
 
 	if( $gBitSystem->isPackageActive( 'newsletters' ) ) {
 		require_once( NEWSLETTERS_PKG_PATH.'BitMailer.php' );
