b48e0d1dde008761b1eee3341afe19a4195d6fad
Christian Fowler <spider@viovio.com>
2009-04-15 02:24:49 +0000

    add users_expunge service method to clear out anything in the mail_queue

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -1,6 +1,8 @@
 <?php
 global $gBitSystem;
 
+define( 'LIBERTY_SERVICE_NEWSLETTERS', 'newsletters' );
+
 $registerHash = array(
 	'package_name' => 'newsletters',
 	'package_path' => dirname( __FILE__ ).'/',
@@ -18,6 +20,19 @@ if( $gBitSystem->isPackageActive( NEWSLETTERS_PKG_NAME ) ) {
 	if( isset( $_GET['ct'] ) && strlen( $_GET['ct'] ) == 32 ) {
 		require_once( NEWSLETTERS_PKG_PATH.'BitNewsletterMailer.php' );
 		BitNewsletterMailer::storeClickthrough( $_GET['ct'] );
+	}
+
+	$gLibertySystem->registerService( LIBERTY_SERVICE_NEWSLETTERS, TAGS_PKG_NAME, array(
+			'users_expunge_function'	=> 'newsletters_user_expunge',
+	) );
+
+	// make sure all mail_queue messages from a deleted user are nuked
+	function newsletters_user_expunge( &$pObject ) {
+		if( is_a( $pObject, 'BitUser' ) && !empty( $pObject->mUserId ) ) {
+			$pObject->mDb->StartTrans();
+			$pObject->mDb->query( "DELETE FROM `".BIT_DB_PREFIX."mail_queue` WHERE user_id=?", array( $pObject->mUserId ) );
+			$pObject->mDb->CompleteTrans();
+		}
 	}
 }
 ?>
