0bc6f589dd5b3d091fff781f9173c8ec86a11261
lsces <lester@lsces.co.uk>
2013-07-11 22:18:53 +0100

    Docblock header added

--- user_register_inc.php
+++ user_register_inc.php
@@ -1,5 +1,18 @@
 <?php
+/**
+ * Copyright (c) 2005 bitweaver.org
+ * All Rights Reserved. See below for details and a complete list of authors.
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See http://www.gnu.org/copyleft/lesser.html for details
+ *
+ * created 2005/12/10
+ *
+ * @package newsletters
+ * @author spider <spider@steelsun.com>
+ */
 
+/** 
+ * Initialization
+ */
 require_once NEWSLETTERS_PKG_PATH.'BitNewsletter.php';
 
 $listHash = array( 'registration_optin' => TRUE );
